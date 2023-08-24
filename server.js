df71bf51965214121234c7a6fdb0c838dde6c2a8
Taylor Treadwell <taylortread91@gmail.com>
2023-08-24 13:48:17 -0500

    magic eightball created

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 const PORT = 3000;
 
+const answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
 
 app.get('/' ,(req,res)=>{
@@ -22,7 +23,9 @@ app.get('/tip/:t1/:t2', (req,res)=>{
     res.send('Tip: ' + req.params.t1 * req.params.t2 / 100)
 })
 
-
+app.get('/magic/:question', (req,res) => {
+    res.send(`${req.params.question}:` + `` + `<h1>` + answers[(Math.floor(Math.random() * answers.length+1))]+ `</h1>`)
+} )
 
 app.listen(PORT, function () {
     console.log(`Listening on port ${PORT}`);
